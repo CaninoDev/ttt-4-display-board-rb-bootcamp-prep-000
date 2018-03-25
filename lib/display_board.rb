25970dd0c387815d5f0a84f5b62e1bf6ad80d251
Ian Canino <ian.canino@gmail.com>
2018-03-25 18:53:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,9 @@
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
 def define_board(board)
-  puts "#{board[0]} | #{board[0]} | #{board[0]}"
-  puts """
-  
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+end
